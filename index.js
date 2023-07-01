ac50e787126b8ade5c541b5f863ece3d7993912d
Zviad Kvaratskhelia <kvara007@gmail.com>
2023-07-01 22:27:55 +0400

    update html

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const questionsArea = document.querySelectorAll(".question");
 
-Array.from(questionsArea).forEach(question => {
+questionsArea.forEach(question => {
   question.addEventListener("click", () => {
     question.parentElement.classList.toggle("active");
-    questionsArea.forEach(question2 => {
-      if (question != question2) {
-        question2.parentElement.classList.remove("active");
+    questionsArea.forEach(question1 => {
+      if (question != question1) {
+        question1.parentElement.classList.remove("active");
       }
     });
   });
 });
+
+
